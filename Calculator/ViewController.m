c8831b1d10c5d574c02d9b9483a529f99b7e09e3
Syngmaster <max6361@mail.ru>
2017-04-26 12:07:47 +0100

    Second commit

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -9,9 +9,25 @@
 #import "ViewController.h"
 #import "CustomButton.h"
 
+typedef NS_ENUM(NSInteger, CalculatorOperation) {
+    
+    CalculatorOperationAdd = 10,
+    CalculatorOperationSubtract,
+    CalculatorOperationMultiply,
+    CalculatorOperationDivide
+    
+};
+
 @interface ViewController ()
 
 @property (assign, nonatomic) CGFloat number;
+
+@property (assign, nonatomic) CGFloat result;
+
+@property (assign, nonatomic) BOOL operationFlag;
+@property (assign, nonatomic) BOOL resultFlag;
+@property (assign, nonatomic) NSInteger operation;
+
 
 @end
 
@@ -30,34 +46,121 @@
 }
 
 
+- (void)printResult {
+
+
+    self.resultLabel.text = [NSString stringWithFormat:@"%.1f",self.result];
+    
+}
+
 - (IBAction)buttonPressed:(UIButton *)sender {
     
-    self.number = sender.tag + self.number*10;
-    self.resultLabel.text = [NSString stringWithFormat:@"%ld",(long)self.number];
+    if (self.resultFlag) {
+        //self.number = self.result;
+        self.result = 0;
+        self.resultFlag = NO;
+    }
     
+    self.result = sender.tag + self.result*10;
+    [self printResult];
+
 }
 
 - (IBAction)equalButton:(UIButton *)sender {
     
-    
+    if (self.operationFlag && !self.resultFlag) {
+        
+        switch (self.operation) {
+            case CalculatorOperationAdd:
+                self.result = self.number + self.result;
+                break;
+            case CalculatorOperationSubtract:
+                self.result = self.number - self.result;
+                break;
+            case CalculatorOperationMultiply:
+                self.result = self.number * self.result;
+                break;
+            case CalculatorOperationDivide:
+                self.result = self.number / self.result;
+                break;
+                
+        }
+        [self printResult];
+        self.operationFlag = NO;
+    }
 }
 
 - (IBAction)operationButton:(UIButton *)sender {
     
-    
+    if (self.operationFlag && !self.resultFlag) {
+        
+        switch (self.operation) {
+            case CalculatorOperationAdd:
+                self.result = self.number + self.result;
+                break;
+            case CalculatorOperationSubtract:
+                self.result = self.number - self.result;
+                break;
+            case CalculatorOperationMultiply:
+                self.result = self.number * self.result;
+                break;
+            case CalculatorOperationDivide:
+                self.result = self.number / self.result;
+                break;
+
+        }
+        [self printResult];
+
+    }
+    self.number = self.result;
+
+    self.resultFlag = YES;
+    self.operationFlag = YES;
+    self.operation = sender.tag;
 }
 
 - (IBAction)clearButton:(UIButton *)sender {
     
-    self.resultLabel.text = @"0";
     self.number = 0.f;
+    self.result = 0;
+    
+    self.resultFlag = NO;
+    self.operationFlag = NO;
+    
+    [self printResult];
+
     
 }
 
 - (IBAction)negativeSignButton:(UIButton *)sender {
+    
+    if (self.result > 0) {
+        
+        self.result = 0 - self.result;
+        [self printResult];
+        
+    } else {
+        
+        self.result = - self.result;
+        [self printResult];
+
+    }
+    
+    if (self.result == 0) {
+        
+        self.result = self.result - self.result;
+        [self printResult];
+
+    }
+    
 }
 
 - (IBAction)percentButton:(UIButton *)sender {
+    
+    self.result = 0.01 * self.result;
+    [self printResult];
+    
+    
 }
 
 
